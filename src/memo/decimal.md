89c7b2cc8f8bb67e00114456cb8b1e2a5f838444
Taku2515 <it.program.2357@gmail.com>
2025-12-06 21:24:05 +0900

    feat: いろいろ整理

---
+++ src/memo/decimal.md
@@ -0,0 +1,5 @@
+小数点以下の表示する桁数について
+---
+```
+cout << fixed << setprecision(15) << endl;
+```
--- src/template/template.hpp
+++ src/template/template.hpp
@@ -1,14 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 using ll = long long;
+using VLL = vector<ll>;
+using VVLL = vector<VLL>;
 const ll INF = LLONG_MAX / 4;
-#define rep(i, a, b) for(ll i = a; i < (b); i++)
+#define rep(i, n) for(ll i = 0; i < ll(n); ++i)
 #define all(a) begin(a), end(a)
-#define sz(a) ssize(a)
 bool chmin(auto& a, auto b) { return a > b ? a = b, 1 : 0; }
 bool chmax(auto& a, auto b) { return a < b ? a = b, 1 : 0; }
 
-int main() {
-   cin.tie(0)->sync_with_stdio(0);
-   // your code here...
-}
+int main() { cout << fixed << setprecision(15) << endl; }
